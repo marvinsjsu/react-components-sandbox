279d820c9acd57e9a0f5cc78bfdb40d73b27dcd9
marvin mante <marvinsjsu@gmail.com>
2018-12-27 14:08:38 -0800

    update presearch-react-components

--- src/App.js
+++ src/App.js
@@ -19,19 +19,25 @@ class App extends Component {
       >
         <h4>Auth component (Sign-in/Sign-up flow, will need to have CORs plugin similar to
           <a href="https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en">
-            this
+            {" "}this
           </a>)
         </h4>
         <Auth />
 
+        <br/>
+        <br/>
         <hr/>
         <h4>Password Update component</h4>
         <PasswordUpdate />
 
+        <br/>
+        <br/>
         <hr/>
         <h4>Authentication Setup</h4>
         <AuthenticatorSetup />
 
+        <br/>
+        <br/>
         <hr/>
         <h4>Authentication Disable</h4>
         <AuthenticatorDisable />
