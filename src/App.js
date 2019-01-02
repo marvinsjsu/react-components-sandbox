42027902a72b50a9175c7a446a4b443576e7bed6
marvin mante <marvinsjsu@gmail.com>
2019-01-02 14:55:26 -0800

    latest auth package

--- src/App.js
+++ src/App.js
@@ -1,46 +1,76 @@
 import React, { Component } from 'react';
-import { Login, Register, withAuth, Auth, AuthCode, AuthenticatorSetup, AuthenticatorDisable, PasswordReset, PasswordUpdate } from 'presearch-react-components';
+import { Login, Register, withAuth, Auth, AuthCode, AuthenticatorSetup, AuthenticatorDisable, PasswordReset, PasswordUpdate } from 'react-components';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: 'authenticator_disable',
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(display) {
+    console.log('display: ', display);
+    this.setState({
+      display: display
+    });
+  }
+
+
   render() {
 
+    const { display } = this.state;
     const { login, getPasswordStrength } = this.props;
-
-    console.log("PASSWORD STRENGTH: ", getPasswordStrength('a123c'));
-
-    console.log("TYPEOF LOGIN: ", typeof login);
-    console.log("LOGIN: ", login);
 
     return (
       <div
         className="App"
-        style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 20}}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 20
+        }}
       >
-        <h4>Auth component (Sign-in/Sign-up flow, will need to have CORs plugin similar to
+        <h4>
+          Auth component (Sign-in/Sign-up flow, will need to have CORs plugin similar to
           <a href="https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en">
             {" "}this
           </a>)
         </h4>
-        <Auth />
+        <Auth initialDisplayComponent={'login'}/>
 
         <br/>
         <br/>
-        <hr/>
-        <h4>Password Update component</h4>
-        <PasswordUpdate />
+        <br/>
+
+        <h4>
+          Password Reset
+        </h4>
+        <Auth initialDisplayComponent={'password_update'}/>
 
         <br/>
         <br/>
-        <hr/>
-        <h4>Authentication Setup</h4>
-        <AuthenticatorSetup />
+        <br/>
+
+        <h4>
+          Setup Authenticator
+        </h4>
+        <Auth initialDisplayComponent={'authenticator_setup'}/>
 
         <br/>
         <br/>
-        <hr/>
-        <h4>Authentication Disable</h4>
-        <AuthenticatorDisable />
+        <br/>
+
+        <h4>
+          Disable Authenticator
+        </h4>
+        <Auth initialDisplayComponent={'authenticator_disable'}/>
       </div>
     );
   }
